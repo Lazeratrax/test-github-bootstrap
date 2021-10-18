9cd619f8dd52b823610147f1de8101cf2b569f57
lazeratrax <lazeratrax@gmail.com>
2021-10-18 15:07:59 +0300

    init components

---
+++ src/app/nav-basic/nav-basic.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBasicComponent } from './nav-basic/nav-basic.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavBasicComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavBasicModule { }
